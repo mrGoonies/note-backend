e00b4304f43f62ecab1963103b5f0f299d29910f
mrGoonies <gooniesdev@gmail.com>
2025-10-01 15:26:57 -0300

    Accessing and Storing GCP Access

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
 
+# load enviorment variable
+load_dotenv(".env.local")
+
+
 class Settings:
     GCP_PROJECT_ID: str = os.getenv("GCP_PROJECT_ID")
     FIRESTORE_DATABASE_ID: str = os.getenv("FIRESTORE_DATABASE_ID")
