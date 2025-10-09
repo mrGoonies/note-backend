2882c2421ae3455d28667c732690aaed4edb3f86
mrGoonies <gooniesdev@gmail.com>
2025-10-09 12:01:20 -0300

    Modify gitignore and apply types and docs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from app.client.firestore import get_firestore_client
+from typing import Dict, List
 
 app = FastAPI()
 
 @app.get('/')
-def index():
+def index() -> Dict[str, str]:
+    """
+    Simple JSON return for testing FastAPI.
+    :return: Dictionary with string values with a message
+    """
     return {"Message": "Hello, World with FastAPI"}
 
 
 @app.get('/notes')
-async def get_notes():
+async def get_notes() -> Dict[str,List[str]]:
+    """
+    Get all notes stored in Firestore
+    :return Dict[str,List[str]]: Dictionary of notes stored in Firestore
+    """
     db = get_firestore_client()
     collection_ref = db.collection("test-notes")
 
